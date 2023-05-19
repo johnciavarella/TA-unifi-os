62f92851aefa5e8d76b89beffbcc153f4d366254
James Hodgkinson <james@terminaloutcomes.com>
2023-05-20 09:46:07 +1000

    fixing package script again!

--- scripts/package.sh
+++ scripts/package.sh
@@ -11,20 +11,20 @@ echo "Copying files to temp dir for packaging..."
 rm -rf
 mkdir -p "${APP_PACKAGE_TEMP}"
 rsync -a --exclude '*/__pycache__/*' \
-    --exclude "./scripts/*" \
-    --exclude "./.github" \
-    --exclude "./.git" \
-    --exclude "./*.tgz" \
+    --exclude "scripts/" \
+    --exclude ".github" \
+    --exclude ".git*" \
+    --exclude "*.tgz" \
     . "${APP_PACKAGE_TEMP}"
 
 echo "Creating ${APP_PACKAGE}.tgz"
 COPYFILE_DISABLE=1 tar czf "./${APP_PACKAGE}.tgz" \
     -C "${TEMPORARY_DIR}" \
     --exclude '*/__pycache__/*' \
-    --exclude "./scripts/*" \
-    --exclude "./.github" \
-    --exclude "./.git" \
-    --exclude "./*.tgz" \
+    --exclude "scripts/" \
+    --exclude ".github" \
+    --exclude ".git*" \
+    --exclude "*.tgz" \
     "${APP_PACKAGE}"
 
 echo "Done, listing files"
